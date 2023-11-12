978d86da7c6610ce8ff8fc18f55220b8e98b80ca
Edison Mayorga <mayorga.sebas100@hotmail.com>
2023-11-11 19:28:16 -0500

    Postres principales

--- src/app/postres/postres.component.ts
+++ src/app/postres/postres.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Postre {
+  nombre: string;
+  precio: string;
+  foto: string;
+  descripcion: string;
+}
+
 @Component({
   selector: 'app-postres',
   templateUrl: './postres.component.html',
   styleUrls: ['./postres.component.css']
 })
 export class PostresComponent {
+
+  postres: Postre[] = [
+    { nombre: 'Natas', precio: '7000', foto: 'Postres 1.png', descripcion: '' },
+    { nombre: 'Vaina', precio: '7000', foto: 'Postres 1.png', descripcion: '' },
+    { nombre: 'Cereales', precio: '7000', foto: 'Postres 1.png', descripcion: '' },
+    { nombre: 'Delicias', precio: '7000', foto: 'Postres 1.png', descripcion: '' },
+    { nombre: 'Primero', precio: '7000', foto: 'Postres 1.png', descripcion: '' },
+    { nombre: 'Primero', precio: '7000', foto: 'Postres 1.png', descripcion: '' },
+   
+  ];
+
+  constructor() {
+    
+  }
 
 }
