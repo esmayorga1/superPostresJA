dbe7c26d45e936733126e78c85831d76d4d67610
Edison Mayorga <mayorga.sebas100@hotmail.com>
2023-11-14 10:20:33 -0500

    Ajustes de la pagina detalle producto

--- src/app/producto-detalle/producto-detalle.component.ts
+++ src/app/producto-detalle/producto-detalle.component.ts
@@ -17,16 +17,55 @@ interface Postre {
 export class ProductoDetalleComponent {
 
   postres: Postre[] = [
-    { nombre: 'Natas', precio: '7000', foto: 'Postres 1.png', descripcion: '', id: 1 },
-    { nombre: 'Vaina', precio: '7000', foto: 'Postres 1.png', descripcion: '', id: 2 },
-    { nombre: 'Cereales', precio: '7000', foto: 'Postres 1.png', descripcion: '', id: 3 },
-    { nombre: 'Delicias', precio: '7000', foto: 'Postres 1.png', descripcion: '', id: 4 },
-    { nombre: 'Primero', precio: '7000', foto: 'Postres 1.png', descripcion: '', id: 5 },
-    { nombre: 'Primero', precio: '7000', foto: 'Postres 1.png', descripcion: '', id: 6 },
+    { nombre: 'Natas', precio: '7000', foto: 'Postres 1.png', descripcion: 'Un postre de natas es una deliciosa creación culinaria que destaca por su suavidad y cremosidad', id: 1 },
+    { nombre: 'Vaina', precio: '7000', foto: 'Postres 1.png', descripcion: 'Un postre de natas es una deliciosa creación culinaria que destaca por su suavidad y cremosidad', id: 2 },
+    { nombre: 'Cereales', precio: '7000', foto: 'Postres 1.png', descripcion: 'Un postre de natas es una deliciosa creación culinaria que destaca por su suavidad y cremosidad', id: 3 },
+    { nombre: 'Delicias', precio: '7000', foto: 'Postres 1.png', descripcion: 'Un postre de natas es una deliciosa creación culinaria que destaca por su suavidad y cremosidad', id: 4 },
+    { nombre: 'Primero', precio: '7000', foto: 'Postres 1.png', descripcion: 'Un postre de natas es una deliciosa creación culinaria que destaca por su suavidad y cremosidad', id: 5 },
+    { nombre: 'Primero', precio: '7000', foto: 'Postres 1.png', descripcion: 'Un postre de natas es una deliciosa creación culinaria que destaca por su suavidad y cremosidad', id: 6 },
+  ];
+
+  postre1 = [
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+  ];
+
+  postre2 = [
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+  ];
+
+  postre3 = [
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+  ];
+
+  postre4 = [
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+  ];
+
+  postre5 = [
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+  ];
+
+  postre6 = [
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
+    { foto: 'Postres 1.png' },
   ];
 
   postId: number = 0;
+  fotoPostreSeleccionado: { foto: string }[] | undefined;
   postreSeleccionado: Postre | undefined;
+
+  idPostreFoto = 'postre' + this.postId;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -34,14 +73,41 @@ export class ProductoDetalleComponent {
     this.route.params.subscribe(params => {
       this.postId = +params['id'];
       this.consultarPostrePorId();
+      this.consultarFotosPostrePorId();
     });
   }
 
   consultarPostrePorId() {
     this.postreSeleccionado = this.postres.find(postre => postre.id === this.postId);
-    // Puedes realizar la lógica adicional de consulta aquí, por ejemplo, llamar a un servicio HTTP.
-    // this.miServicio.consultarPostrePorId(this.postId).subscribe(resultado => {
-    //   this.postreSeleccionado = resultado;
-    // });
+    this.idPostreFoto = 'postre' + this.postId;
+  }
+
+  consultarFotosPostrePorId() {
+    if (this.postreSeleccionado) {
+      // Asignar el arreglo correspondiente según el postId
+      switch (this.postId) {
+        case 1:
+          this.fotoPostreSeleccionado = this.postre1;
+          break;
+        case 2:
+          this.fotoPostreSeleccionado = this.postre2;
+          break;
+        case 3:
+          this.fotoPostreSeleccionado = this.postre3;
+          break;
+        case 4:
+          this.fotoPostreSeleccionado = this.postre4;
+          break;
+        case 5:
+          this.fotoPostreSeleccionado = this.postre5;
+          break;
+        case 6:
+          this.fotoPostreSeleccionado = this.postre6;
+          break;
+        default:
+          this.fotoPostreSeleccionado = [];
+          break;
+      }
+    }
   }
 }
